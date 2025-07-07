b62b5e4de3c3a906928f1d4b257d745d64d0612c
lapiks <dhenin.julien@gmail.com>
2025-07-07 22:17:46 +0200

    test taichi

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import taichi as ti
+
+ti.init()
+
+pixels = ti.field(ti.u8, shape=(512, 512, 3))
+
+@ti.kernel
+def set_pixels():
+    for i, j, k in pixels:
+        pixels[i, j, k] = ti.random() * 255
+
+set_pixels()
+filename = f'image.png'
+ti.tools.imwrite(pixels.to_numpy(), filename)
+print(f'The image has been saved to {filename}')
