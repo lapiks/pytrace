085eea60ea318a7a569d006d290c676f1c8457d6
lapiks <dhenin.julien@gmail.com>
2025-07-09 11:47:02 +0200

    vec3

---
+++ src/vec3.py
@@ -0,0 +1,11 @@
+import taichi.math as tm
+
+Vec3 = tm.vec3
+Point3 = tm.vec3
+Color = tm.vec3
+
+WHITE = Color(1.0, 1.0, 1.0)
+BLACK = Color(0.0, 0.0, 0.0)
+RED = Color(1.0, 0.0, 0.0)
+GREEN = Color(0.0, 1.0, 0.0)
+BLUE = Color(0.0, 0.0, 1.0)
