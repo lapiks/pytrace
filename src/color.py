d4c93f8766441a1ca17e86067e3da28bd8faf310
lapiks <dhenin.julien@gmail.com>
2025-07-09 11:48:05 +0200

    color

---
+++ src/color.py
@@ -0,0 +1,9 @@
+import taichi.math as tm
+
+Color = tm.vec3
+
+WHITE = Color(1.0, 1.0, 1.0)
+BLACK = Color(0.0, 0.0, 0.0)
+RED = Color(1.0, 0.0, 0.0)
+GREEN = Color(0.0, 1.0, 0.0)
+BLUE = Color(0.0, 0.0, 1.0)
